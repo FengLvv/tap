2d817cfe162ae1994dcbd728b2a8eb0013cc798e
fenglvv <1260386538@qq.com>
2024-01-17 13:55:04 +0100

    hollow world

--- tap/Program.cs
+++ tap/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace HelloWorldApplication
+{
+    /* 类名为 HelloWorld */
+    class HelloWorld
+    {
+        /* main函数 */
+        static void Main(string[] args)
+        {
+            /* 我的第一个 C# 程序 */
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
