17f1e516f98cbae4d48def76d33207fe6d43cada
maqingyuan <18377237@buaa.edu.cn>
2024-01-17 15:16:19 +0100

    gameobj

--- tap/GameObj.cs
+++ tap/GameObj.cs
@@ -5,7 +5,61 @@ namespace HelloWorldApplication;
 public abstract class GameObj
 {
     protected Vector2 pos;
-    protected int[,] pattern; 
+    protected float[,] pattern; 
+    protected int movespeed;
     protected void OnStart() { }
     protected void Update(){}
+}
+
+public class Customer::GameObj
+{
+    private int liveflag;
+
+    private void automove(){
+        pos += vector2(-1,0) * movespeed;
+    }
+
+    private void getbeer(){
+        liveflag = 0;
+    }
+
+}
+
+public class Player::GameObj
+{
+    private void move(){
+        if (Console.KeyAvailable)
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            switch (key)
+            {
+                case ConsoleKey.W:
+                    pos += new Vector2(0, -1) * movespeed;
+                    break;
+                case ConsoleKey.A:
+                    pos += new Vector2(-1, 0) * movespeed;
+                    break;
+                case ConsoleKey.S:
+                    pos += new Vector2(0, 1) * movespeed;
+                    break;
+                case ConsoleKey.D:
+                    pos += new Vector2(1, 0) * movespeed;
+                    break;
+
+                case ConsoleKey.L:
+                    sendbeer();
+                    break;
+            }
+        }
+    }
+
+    private void sendbeer(){
+
+        Beer beer = new Beer(pos, );
+    }
+}
+
+public class Beer::GameObj(){
+    
 }
