abdaaa3e4e868d3e84dcb7649e973e13a470578c
fenglvv <1260386538@qq.com>
2024-01-17 14:49:35 +0100

    Update GlobalVariable

---
+++ tap/GlobalVariable.cs
@@ -0,0 +1,9 @@
+namespace HelloWorldApplication;
+
+public static class GlobalVariables
+{
+    public static float dt = 0.1f;
+    public static float[,] Triangle = new float[3, 3];
+    public static float[,] Rectangle = new float[3, 3];
+    public static float[,] Circle = new float[3, 3];
+}
